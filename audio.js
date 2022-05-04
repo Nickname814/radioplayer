070dae943d781198cd6668eca904b7e3d1d8e5e0
Seungjae Oh <brain0620osj@gmail.com>
2022-05-05 03:53:26 +0900

    second commit-1

--- audio.js
+++ audio.js
@@ -11,6 +11,44 @@ const playBtn = document.querySelector('.play-btn');
 
 
 playBtn.addEventListener('click', () =>{
+    if(playBtn.className.includes('pause')){
+        music.play();
+    }
+    else{
+        music.pause();
+    }
     playBtn.classList.toggle('pause');
     disk.classList.toggle('play');
 })
+
+
+const setAudio =(i) => {
+    seekBar.value = 0;
+    let audio = audios[i];
+    currentAudio = i;
+    audio.src = audio.path;
+
+    audioName.innerHTML = audio.name;
+    writer.innerHTML = audio.writer;
+    disk.style.backgroundImage = `url('${audio.cover})`;
+
+    currentTime.innerHTML = '00:00';
+    setTimeout(() =>{
+        seekBar.max = audio.duration;
+        musicDuration.innerHTML = formatTime(audio.duration);
+    }, 300);
+}
+
+setAudio(0);
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if(min < 10){
+        min = `0${min}`;
+    }
+    let sec = Math.floor(time % 60);
+    if(sec < 10){
+        sec = `0${sec}`;
+    }
+    return `${min} : ${sec}`;
+}
---
+++ data.js
@@ -0,0 +1,8 @@
+let audios = [
+    {
+        name: '오발탄',
+        path: '오디오북 주소',
+        artist: '이범선',
+        cover: '커버링크',
+    }
+]
